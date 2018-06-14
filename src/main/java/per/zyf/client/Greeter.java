67804208fc441805960ae697b884e4e0e9080e55
glorychou <fightzyf1992@hotmail.com>
2018-06-14 23:16:45 +0800

    akka sample

---
+++ src/main/java/per/zyf/client/Greeter.java
@@ -0,0 +1,49 @@
+package per.zyf.client;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+/**
+ * @Author: Finance Group
+ * @Date: 2018/6/14 15:30
+ */
+public class Greeter extends AbstractActor {
+    static public Props props(String message, ActorRef printerActor) {
+        return Props.create(Greeter.class, () -> new Greeter(message, printerActor));
+    }
+
+    static public class WhoToGreet {
+        public final String who;
+
+        public WhoToGreet(String who) {
+            this.who = who;
+        }
+    }
+
+    static public class Greet {
+        public Greet() {
+        }
+    }
+
+    private final String message;
+    private final ActorRef printerActor;
+    private String greeting = "";
+
+    public Greeter(String message, ActorRef printerActor) {
+        this.message = message;
+        this.printerActor = printerActor;
+    }
+
+    @Override
+    public AbstractActor.Receive createReceive() {
+        return receiveBuilder()
+                .match(WhoToGreet.class, wtg -> {
+                    this.greeting = message + ", " + wtg.who;
+                })
+                .match(Greet.class, x -> {
+                    printerActor.tell(new Printer.Greeting(greeting), getSelf());
+                })
+                .build();
+    }
+}
---
+++ src/main/java/per/zyf/client/Printer.java
@@ -0,0 +1,36 @@
+package per.zyf.client;
+
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+/**
+ * @Author: Finance Group
+ * @Date: 2018/6/14 15:38
+ */
+public class Printer extends AbstractActor {
+    static public Props props() {
+        return Props.create(Printer.class, () -> new Printer());
+    }
+
+    static public class Greeting {
+        public final String message;
+
+        public Greeting(String message) {
+            this.message = message;
+        }
+    }
+
+    private LoggingAdapter log = Logging.getLogger(getContext().getSystem(), this);
+
+    public Printer() {
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder().match(Greeting.class, greeting -> {
+            log.info(greeting.message);
+        }).build();
+    }
+}
