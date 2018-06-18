4d7060b5c1645a184960ff2638bfb49a7c6b2c58
glorychou <fightzyf1992@hotmail.com>
2018-06-18 23:51:05 +0800

    modify description

--- Deal/src/main/java/com/akkasample/deal/AppMain.java
+++ Deal/src/main/java/com/akkasample/deal/AppMain.java
@@ -11,11 +11,11 @@ public class AppMain {
     public static void main(String[] args) {
         final ActorSystem system = ActorSystem.create();
         try {
-            final ActorRef guest1 = system.actorOf(AccountActor.props(100), "guest1");
-            final ActorRef guest2 = system.actorOf(AccountActor.props(100), "guest2");
+            final ActorRef accountA = system.actorOf(AccountActor.props(100), "accountA");
+            final ActorRef accountB = system.actorOf(AccountActor.props(100), "accountB");
 
             final ActorRef cooperator = system.actorOf(CooperatorActor.props(), "cooperator");
-            cooperator.tell(new CooperatorActor.TransferMsg(guest1, guest2, 100), ActorRef.noSender());
+            cooperator.tell(new CooperatorActor.TransferMsg(accountA, accountB, 100), ActorRef.noSender());
 
             System.out.println(">>> Press ENTER to exit <<<");
             System.in.read();
